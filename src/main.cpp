4ee842165d1d31cfd679a444fe0c2d421fa06e3c
lazyaff <faizalanri15@gmail.com>
2023-06-15 17:51:08 +0700

    konfigurasi kontrol servo v.2

--- src/main.cpp
+++ src/main.cpp
@@ -17,19 +17,11 @@ int servoPin = D1; // Pin servo terhubung ke D1 (GPIO5) di NodeMCU
 
 RTC_DS1307 rtc;
 
-void page()
-{
-  String html = "<html><head><title>Fish Feeder</title><link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM' crossorigin='anonymous'></head>";
-  html += "<body class='text-center'><h1>Fish Feeder</h1>";
-  html += "<p>Tekan tombol dibawah untuk memberi makan ikan.</p>";
-  html += "<p><a href=\"servo\"\"><button class='btn btn-primary'>Mulai</button></a></p>";
-  html += "</body></html>";
-  server.send(200, "text/html", html);
-}
+String html;
 
 void handleRoot()
 {
-  page();
+  server.send(200, "text/html", html);
 }
 
 void handleServo()
@@ -37,28 +29,35 @@ void handleServo()
   myservo.write(180);
   delay(1000); // Berikan waktu untuk servo mencapai posisi yang diinginkan
   myservo.write(0);
-  page();
+  server.send(200, "text/html", html);
 }
 
 void setup()
 {
   Serial.begin(9600);
+
+  myservo.attach(D1);
+  myservo.write(90); // Posisi awal servo (0 derajat)
+
   WiFi.mode(WIFI_AP);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(1000);
     Serial.println("");
     Serial.print("Connecting to WiFi...");
+    delay(1000);
   }
   Serial.println("");
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  myservo.attach(D1);
-  myservo.write(0); // Posisi awal servo (0 derajat)
+  html += "<html><head><title>Fish Feeder</title><link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM' crossorigin='anonymous'></head>";
+  html += "<body class='text-center'><h1>Fish Feeder</h1>";
+  html += "<p>Tekan tombol dibawah untuk memberi makan ikan.</p>";
+  html += "<p><a href=\"servo\"\"><button class='btn btn-primary'>Mulai</button></a></p>";
+  html += "</body></html>";
 
   server.on("/", handleRoot);
   server.on("/servo", handleServo);
@@ -75,6 +74,16 @@ void setup()
 void loop()
 {
   server.handleClient();
+  // for (int pos = 0; pos <= 180; pos += 1)
+  // {                     // Menggerakkan servo dari 0 derajat hingga 180 derajat
+  //   myservo.write(pos); // Menggerakkan servo ke posisi tertentu
+  //   delay(15);          // Delay 15ms sebelum menggerakkan servo ke posisi selanjutnya
+  // }
+  // for (int pos = 180; pos >= 0; pos -= 1)
+  // {                     // Menggerakkan servo dari 180 derajat hingga 0 derajat
+  //   myservo.write(pos); // Menggerakkan servo ke posisi tertentu
+  //   delay(15);          // Delay 15ms sebelum menggerakkan servo ke posisi selanjutnya
+  // }
 
   // DateTime now = rtc.now();
   // int currentHour = now.hour();
